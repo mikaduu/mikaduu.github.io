267275c9230d700d340b503699b9ae9f35069c09
mikaduu <2818085254@qq.com>
2024-10-27 21:44:43 +0800

    Add files via upload

--- form_demo/static/js/script.js
+++ form_demo/static/js/script.js
@@ -5,15 +5,19 @@ const menu = document.getElementById('menu');
 // 为菜单图标添加点击事件
 menuIcon.addEventListener('click', (event) => {
     //menuIcon.classList.toggle('active');
-    if (menu.classList.contains('active')) {
-        menu.classList.remove('active');
-        menuContainer.classList.remove('active');
-        menuIcon.classList.remove('active');
-    } else {
-        menu.classList.add('active'); 
-        menuContainer.classList.add('active');
-        menuIcon.classList.add('active');
-    }
+    // if (menu.classList.contains('active')) {
+    //     menu.classList.remove('active');
+    //     menuContainer.classList.remove('active');
+    //     menuIcon.classList.remove('active');
+    // } else {
+    //     menu.classList.add('active'); 
+    //     menuContainer.classList.add('active');
+    //     menuIcon.classList.add('active');
+    // }
+    
+    menu.classList.toggle('active'); 
+    menuContainer.classList.toggle('active'); // 添加此行以确保菜单容器的状态切换
+    menuIcon.classList.toggle('active');
     // 阻止事件冒泡
     event.stopPropagation();
 });
@@ -30,3 +34,19 @@ document.addEventListener('click', (event) => {
         } 
     }
 });
+
+window.onscroll = function() {
+    const navbar = document.getElementById("navbar");
+    const scrollDistance = 570; // 设置滚动距离
+
+    if (document.body.scrollTop > scrollDistance || document.documentElement.scrollTop > scrollDistance) {
+        navbar.classList.add("fixed"); // 添加fixed类
+    } else {
+        navbar.classList.remove("fixed"); // 移除fixed类
+        if (menu.classList.contains('active')) {
+            menu.classList.remove('active');
+            menuContainer.classList.remove('active');
+            menuIcon.classList.remove('active');
+        } 
+    }
+};
